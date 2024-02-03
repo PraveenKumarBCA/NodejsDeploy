0144cdc5dad7d69c0874233573cdf6a7997703cf
Praveen Kumar <praveenkumar21bca05@gmail.com>
2024-02-03 18:07:13 +0530

    init

--- Backend/Appcontrollers/UserController.js
+++ Backend/Appcontrollers/UserController.js
@@ -2,39 +2,36 @@ const UserModel=require("../Appmodels/UserModel");
 const jwt=require("jsonwebtoken");
 const TOKENKEY="PRAVEEN7979";
 module.exports.createUseraccount=async(request,response)=>{
-    let Bodydata=request.body;
-    //   let Userexist=await UserModel.find({email:Bodydata.email});
-    // if(Userexist.length >0){
-    //     response.send({
-    //         status:false,
-    //         messege:"User already Exist"
-    //     });
-    // }
-    // else{
-    // let NewUser=new UserModel({
-    //     Name:Bodydata.Name,
-    //     password:Bodydata.password,
-    //     email:Bodydata.email,
-    //     Phone:Bodydata.Phone,
-    //     address:Bodydata.address
-    // });
-    // let UserResult=await NewUser.save();
-    // if(UserResult){
-    //     response.send({
-    //         status:true,
-    //         messege:"You have been Registered Successfully"
-    //     });
-    // }
-    // else{
-    //     response.send({
-    //         status:false,
-    //         messege:"You Registration was Failed"
-    //     });
-    // }
-//}
-response.send({
-    Bodydata
-})
+      let Userexist=await UserModel.find({email:Bodydata.email});
+    if(Userexist.length >0){
+        response.send({
+            status:false,
+            messege:"User already Exist"
+        });
+    }
+    else{
+    let NewUser=new UserModel({
+        Name:Bodydata.Name,
+        password:Bodydata.password,
+        email:Bodydata.email,
+        Phone:Bodydata.Phone,
+        address:Bodydata.address
+    });
+    let UserResult=await NewUser.save();
+    if(UserResult){
+        response.send({
+            status:true,
+            messege:"You have been Registered Successfully"
+        });
+    }
+    else{
+        response.send({
+            status:false,
+            messege:"You Registration was Failed"
+        });
+    }
+}
+
 };
 module.exports.userLogin=async (request,response)=>{
    let Bodydata=request.body;
